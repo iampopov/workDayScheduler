68a8ddc5fce762e71132c9630bd9a060abf03574
IamPopov <ivantsar@gmail.com>
2019-12-21 20:11:23 -0700

    local storage operational for one block of code

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-var saveBtnEl = document.querySelector(".saveBtn");
+var nineAMbtnEL = document.getElementById("9amBtn");
+var tenAMbtnEL = document.getElementById("10amBtn");
 var toDo = document.querySelector(".description");
 
 function renderToDo () {
-    var textBlock = document.querySelector(".description");
-
-    if (textBlock.value === null) {
+    var text = localStorage.getItem("ToDo");
+    if (text === null) {
         return;
     }
-
-    toDo.textContent = textBlock;
-
+    toDo.setAttribute("value", text);
 }
 
-saveBtnEl.addEventListener("click", function(e) {
+function evt (e) {
     e.preventDefault();
     var textBlock = document.querySelector(".description").value;
     if (textBlock === "") {
         alert("Can't save empty to do");
     } else {
-    
-    localStorage.setItem("ToDo", textBlock)    
-    toDo.textContent = textBlock;
-    renderToDo();
+        localStorage.setItem("ToDo", textBlock)    
+        renderToDo(textBlock);
     }
+};
 
-})
+nineAMbtnEL.addEventListener("click", evt);
+tenAMbtnEL.addEventListener("click", evt);
+
+renderToDo();
