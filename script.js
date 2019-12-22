e378e5c6e805ffea39135840b7cd6a4a4ae750c6
IamPopov <ivantsar@gmail.com>
2019-12-22 15:21:44 -0700

    local storage for multiple events

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 var nineAMbtnEL = document.getElementById("9amBtn");
 var tenAMbtnEL = document.getElementById("10amBtn");
-var toDo = document.querySelector(".description");
+var toDo9am = document.getElementById("nineA")
+var toDo10am = document.getElementById("tenA");
+// var renderToDoEl = "";
 
 function renderToDo () {
-    var text = localStorage.getItem("ToDo");
-    if (text === null) {
-        return;
-    }
-    toDo.setAttribute("value", text);
+    var nineAEl = localStorage.getItem("nineA");
+    var tenAEl = localStorage.getItem("tenA");
+    
+    toDo9am.setAttribute("value", nineAEl);
+    if (nineAEl === null) {
+        toDo9am.setAttribute("value", " ");
+        }
+    toDo10am.setAttribute("value", tenAEl);
+    if (tenAEl === null) {
+        toDo10am.setAttribute("value", " ");
+        }
 }
 
 function evt (e) {
     e.preventDefault();
-    var textBlock = document.querySelector(".description").value;
-    if (textBlock === "") {
-        alert("Can't save empty to do");
-    } else {
-        localStorage.setItem("ToDo", textBlock)    
-        renderToDo(textBlock);
-    }
+    var textBlock = document.querySelector("#"+this.value).value;
+    localStorage.setItem(this.value, textBlock)    
+    renderToDo(textBlock);
 };
-
+console.log(localStorage);
 nineAMbtnEL.addEventListener("click", evt);
 tenAMbtnEL.addEventListener("click", evt);
 
